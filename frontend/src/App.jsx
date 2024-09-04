d5200f726342939afbe0a78480259396641815c4
sai <163710782+sai7siri@users.noreply.github.com>
2024-09-04 12:03:04 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import {BrowserRouter , Routes , Route, Navigate} from "react-router-dom";
 import Home from "./chat_app/components/Home"
 import Signup from "./chat_app/pages/Signup"
-import Signin from "./chat_app/pages/Signin";
+import SignIn from "./chat_app/pages/SignIn";
 import {Toaster} from "react-hot-toast"
 import { useAuthContext } from './chat_app/Context/authUser';
 
@@ -18,9 +18,9 @@ function App() {
 
         <Route path='/' element={<Navigate to="/signin" />} />
 
-        <Route path='/home' element={authUser ? <Home /> : <Navigate to='/signin' /> } />
+        <Route path='/home' element={authUser ? <Home /> : <Navigate to='/signIn' /> } />
         <Route path='/signup' element={authUser ? <Navigate to='/home' /> : <Signup /> } />
-        <Route path='/signin' element={ authUser ? <Navigate to="/home" /> : <Signin /> } />
+        <Route path='/signin' element={ authUser ? <Navigate to="/home" /> : <SignIn /> } />
 
       </Routes>
       </BrowserRouter>
